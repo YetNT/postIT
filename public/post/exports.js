14b51b45797472625c50b1edf694c0bc292d46f1
Yetity <hlonipoole692@gmail.com>
2023-08-19 22:34:39 +0200

    If author only post, restrict access

--- public/post/exports.js
+++ public/post/exports.js
@@ -4,11 +4,19 @@ const { PostMode } = require("../../util");
 
 const get = async (req, res) => {
     const id = req.params.id;
+    const userCookie = req.cookies.user;
 
     try {
         const post = await Post.findOne({ id: id });
+        let user = await User.findOne({
+            id: JSON.parse(userCookie)["_doc"].id,
+        });
 
         if (!post) return res.status(404).send("Post not found");
+        if (!user) user = { id: "some invalid id idk" };
+        if (PostMode.authorPrivate == post.mode && post.authorId !== user.id) {
+            return res.status(405).send("Not allowed.");
+        }
 
         res.render("index", {
             title: post.title,
