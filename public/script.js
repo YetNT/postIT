3be524edb2aebabc340b4ffa3462ab6362f03077
Yetity <hlonipoole692@gmail.com>
2023-08-20 11:13:40 +0200

    even better

--- public/script.js
+++ public/script.js
@@ -1,25 +1,30 @@
 console.log("Script loaded!");
+document.addEventListener("DOMContentLoaded", function () {
+    var hide = true; // Start with true to match initial state of visibility
 
-var hide = true; // Start with true to match initial state of visibility
+    function navbarLineButtonClicked() {
+        var navAs = document.getElementById("topnav").querySelectorAll(".navA");
 
-function navbarLineButtonClicked() {
-    var navAs = document.getElementById("topnav").querySelectorAll(".navA");
-
-    if (hide) {
-        for (let i = 0; i < navAs.length; i++) {
-            navAs[i].style.opacity = 0;
+        if (hide) {
+            for (let i = 0; i < navAs.length; i++) {
+                navAs[i].style.opacity = 0;
+            }
+            document.getElementById("topnav").style.opacity = 0;
+            document.getElementById("navbarBackground").style.backgroundColor =
+                "#424549";
+        } else {
+            for (let i = 0; i < navAs.length; i++) {
+                navAs[i].style.opacity = 1;
+            }
+            document.getElementById("topnav").style.opacity = 1;
+            document.getElementById("navbarBackground").style.backgroundColor =
+                "rgba(25, 25, 25, 0.4)";
         }
-        document.getElementById("topnav").style.opacity = 0;
-        document.getElementById("navbarBackground").style.backgroundColor =
-            "#424549";
-    } else {
-        for (let i = 0; i < navAs.length; i++) {
-            navAs[i].style.opacity = 1;
-        }
-        document.getElementById("topnav").style.opacity = 1;
-        document.getElementById("navbarBackground").style.backgroundColor =
-            "rgba(25, 25, 25, 0.4)";
+
+        hide = !hide; // Toggle the value of hide
     }
 
-    hide = !hide; // Toggle the value of hide
-}
+    document
+        .getElementById("navbarLines")
+        .addEventListener("click", navbarLineButtonClicked);
+});
