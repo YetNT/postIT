6dcb46386bcbed3a635fddc13f7452a795d67854
Yetity <hlonipoole692@gmail.com>
2023-08-20 09:15:15 +0200

    CSS X HTML X JS Navbar
    thanks @FloShutUp for the base of it. Now i can get to work fr fr

--- public/script.js
+++ public/script.js
@@ -1,1 +1,25 @@
-console.log('Script loaded!');
+console.log("Script loaded!");
+
+var hide = true; // Start with true to match initial state of visibility
+
+function navbarLineButtonClicked() {
+    var navAs = document.getElementById("topnav").querySelectorAll(".navA");
+
+    if (hide) {
+        for (let i = 0; i < navAs.length; i++) {
+            navAs[i].style.opacity = 0;
+        }
+        document.getElementById("topnav").style.opacity = 0;
+        document.getElementById("navbarBackground").style.backgroundColor =
+            "#424549";
+    } else {
+        for (let i = 0; i < navAs.length; i++) {
+            navAs[i].style.opacity = 1;
+        }
+        document.getElementById("topnav").style.opacity = 1;
+        document.getElementById("navbarBackground").style.backgroundColor =
+            "rgba(25, 25, 25, 0.4)";
+    }
+
+    hide = !hide; // Toggle the value of hide
+}
