59b4ce371af2b7e0325b1b725219735d83b7d651
Yetity <hlonipoole692@gmail.com>
2023-10-08 22:08:55 +0200

    post discovery
    Note: Upgrade CSS

--- public/home.js
+++ public/home.js
@@ -2,13 +2,14 @@ const { validateSessionFriendly } = require("./_auth/isSignedIn");
 const Post = require("../postSchema");
 const { PostMode } = require("../util");
 
-async function render(req, res, devPosts, newPosts) {
+async function render(req, res, devPosts, newPosts, archivedPosts) {
     const signedIn = await validateSessionFriendly(req);
     res.render("home", {
         signedIn: signedIn.output,
         userId: signedIn.userId,
         devPosts,
-        newPosts
+        newPosts,
+        archivedPosts,
     });
 }
 
@@ -35,12 +36,25 @@ const getDevPosts = async () => {
     }
 };
 
+const getArchivedPosts = async () => {
+    try {
+        const archived = await Post.find({ mode: PostMode.archived })
+            .sort({ created: -1 })
+            .limit(5);
+
+        return archived;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 const get = async (req, res) => {
     try {
-        let devPosts = await getDevPosts()
-        let newPosts = await getNewestPosts()
+        let devPosts = await getDevPosts();
+        let newPosts = await getNewestPosts();
+        let archived = await getArchivedPosts();
 
-        await render(req, res, devPosts, newPosts);
+        await render(req, res, devPosts, newPosts, archived);
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal Server Error");
