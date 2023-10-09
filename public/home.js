15a4cf6a555a86d685307175ef82e04bbb3b81f2
Yetity <hlonipoole692@gmail.com>
2023-10-09 06:57:41 +0200

    post discovery and style, also link to it.

--- public/home.js
+++ public/home.js
@@ -1,6 +1,7 @@
 const { validateSessionFriendly } = require("./_auth/isSignedIn");
 const Post = require("../postSchema");
 const { PostMode } = require("../util");
+var url = require("url");
 
 async function render(req, res, devPosts, newPosts, archivedPosts) {
     const signedIn = await validateSessionFriendly(req);
@@ -13,9 +14,32 @@ async function render(req, res, devPosts, newPosts, archivedPosts) {
     });
 }
 
-const getNewestPosts = async () => {
+function addLink(req, arr) {
+    return arr.map((element) => {
+        element.link = url.format({
+            protocol: req.protocol,
+            host: req.get("host"),
+            pathname: `post/${element.id}`,
+        });
+        return element; // Return the modified element
+    });
+}
+
+const getNewestPosts = async (req) => {
     try {
-        const newestPosts = await Post.find({}).sort({ created: -1 }).limit(5);
+        const posts = await Post.find({
+            mode: {
+                $nin: [
+                    PostMode.authorPrivate,
+                    PostMode.linkPrivate,
+                    PostMode.moderated,
+                ],
+            },
+        })
+            .sort({ created: -1 })
+            .limit(5);
+
+        const newestPosts = addLink(req, posts);
 
         return newestPosts;
     } catch (error) {
@@ -24,11 +48,13 @@ const getNewestPosts = async () => {
     }
 };
 
-const getDevPosts = async () => {
+const getDevPosts = async (req) => {
     try {
-        const devPosts = await Post.find({ mode: PostMode.dev })
+        const posts = await Post.find({ mode: PostMode.dev })
             .sort({ created: -1 })
             .limit(5);
+
+        const devPosts = addLink(req, posts);
 
         return devPosts;
     } catch (error) {
@@ -36,11 +62,13 @@ const getDevPosts = async () => {
     }
 };
 
-const getArchivedPosts = async () => {
+const getArchivedPosts = async (req) => {
     try {
-        const archived = await Post.find({ mode: PostMode.archived })
+        const posts = await Post.find({ mode: PostMode.archived })
             .sort({ created: -1 })
             .limit(5);
+
+        const archived = addLink(req, posts);
 
         return archived;
     } catch (error) {
@@ -50,9 +78,9 @@ const getArchivedPosts = async () => {
 
 const get = async (req, res) => {
     try {
-        let devPosts = await getDevPosts();
-        let newPosts = await getNewestPosts();
-        let archived = await getArchivedPosts();
+        let devPosts = await getDevPosts(req);
+        let newPosts = await getNewestPosts(req);
+        let archived = await getArchivedPosts(req);
 
         await render(req, res, devPosts, newPosts, archived);
     } catch (error) {
